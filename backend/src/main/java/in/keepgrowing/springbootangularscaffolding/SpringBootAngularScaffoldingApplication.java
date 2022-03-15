430f555ddb3e465baa0d681d91abe50a5cbd28c8
b_damlabin <b_damlabin@yahoo.com>
2022-03-15 13:27:36 +0100

    second commit

---
+++ backend/src/main/java/in/keepgrowing/springbootangularscaffolding/SpringBootAngularScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootangularscaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularScaffoldingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAngularScaffoldingApplication.class, args);
+    }
+
+}
